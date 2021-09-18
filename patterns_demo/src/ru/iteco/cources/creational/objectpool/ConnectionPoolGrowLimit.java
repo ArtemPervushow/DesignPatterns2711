8f60c6bacb63309b62e162aa88064f0d6ff56efe
Stolyarenko Alexander <stolyarenkoas@gmail.com>
2021-09-18 23:28:42 +0300

    Implemented pool shrinking; Implemented pooled resource blocking;

--- patterns_demo/src/ru/iteco/cources/creational/objectpool/ConnectionPoolGrowLimit.java
+++ patterns_demo/src/ru/iteco/cources/creational/objectpool/ConnectionPoolGrowLimit.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectionPoolGrowLimit {
+    private static final float shrinkingThreshold = 0.7f;
+    private static final int initialCapacity = 10;
     private static List<ConnectionResource> resourceList = new ArrayList<>();
-    private static volatile short capacity = 10;
+    private static volatile short capacity = initialCapacity;
 
     static {
         for (int i = 0; i < 10; i++) {
@@ -15,16 +17,11 @@ public class ConnectionPoolGrowLimit {
 
     public static ConnectionResource getConnectionFromPool() throws Exception {
         if(resourceList.size() > 0) {
-            ConnectionResource connectionResource = resourceList
-                    .get(resourceList.size() - 1);
-            resourceList.remove(connectionResource);
-
-            return connectionResource;
+            return issueFromPool();
         } else {
             if(capacity <= 20) {
-                ConnectionResource connectionResource = new ConnectionResource();
-                capacity++;
-                return connectionResource;
+                grow();
+                return issueFromPool();
             } else {
                 Thread.sleep(500);
                 return getConnectionFromPool();
@@ -33,6 +30,29 @@ public class ConnectionPoolGrowLimit {
     }
 
     public static void returnConnectionToPool(ConnectionResource connectionResource) {
+        connectionResource.retrieve();
         resourceList.add(connectionResource);
+        if ((float)resourceList.size()/capacity > shrinkingThreshold) {
+            shrink();
+        }
+    }
+
+    private static ConnectionResource issueFromPool() {
+        final ConnectionResource resource = resourceList.remove(resourceList.size() - 1);
+        resource.issue();
+        return resource;
+    }
+
+    private static synchronized void grow() {
+        resourceList.add(new ConnectionResource());
+        capacity++;
+    }
+
+    private static synchronized void shrink() {
+        final int exceedingCount = capacity - initialCapacity;
+        for (int i = 0; i < exceedingCount; i++) {
+            resourceList.remove(resourceList.size() - 1);
+        }
+        capacity = initialCapacity;
     }
 }
