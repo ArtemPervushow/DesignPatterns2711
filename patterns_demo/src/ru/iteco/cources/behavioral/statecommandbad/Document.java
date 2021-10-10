02c03022e6be29a96cc1196de6861f771bddd484
Stolyarenko Alexander <stolyarenkoas@gmail.com>
2021-10-10 15:32:01 +0300

    [HW7] Implemented content addition.

--- patterns_demo/src/ru/iteco/cources/behavioral/statecommandbad/Document.java
+++ patterns_demo/src/ru/iteco/cources/behavioral/statecommandbad/Document.java
@@ -1,7 +1,10 @@
 package ru.iteco.cources.behavioral.statecommandbad;
 
+import java.util.List;
+
 public class Document {
     private DocumentState state;
+    private List<String> content;
 
     public DocumentState getState() {
         return state;
@@ -9,5 +12,11 @@ public class Document {
 
     public void setState(DocumentState state) {
         this.state = state;
+    }
+
+    public void addData(String data) {
+        if (state instanceof NewDocumentState) {
+            content.add(data);
+        }
     }
 }
