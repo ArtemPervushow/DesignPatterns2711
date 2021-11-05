76ed5b2f75dca5cf2324f162171c3173e5e3d318
alexey.kirilchev <alexey.kirilchev@x5.ru>
2021-11-05 17:47:36 +0300

    DP-02 Дополнить класс ConnectionPoolGrowLimit из проекта patterns_demo следующим функциональном: 1) Сделать pool с "восстановлением размера". Т.е. pool растет до определенного лимита (например с 10ти исходных ресурсов до 20ти). Но при снижении нагрузки (т.е. при возврате в pool более чем 70% соединений) удаляет лишние добавленные ресурсы (т.е. уменьшает свой размер с 20ти обратно до 10ти) 2) Добавить запрет использования ресурса из pool-а по ссылке, после того как он был возвращен в pool.

--- patterns_demo/src/ru/iteco/cources/creational/objectpool/ConnectionResource.java
+++ patterns_demo/src/ru/iteco/cources/creational/objectpool/ConnectionResource.java
@@ -3,7 +3,14 @@ package ru.iteco.cources.creational.objectpool;
 import java.util.Random;
 
 public class ConnectionResource {
+
+    private boolean taken = false;
+
     public void work(){
+        if (!taken) {
+            throw new IllegalStateException("Resource is not taken.");
+        }
+
         try {
             System.out.println("Now working " + Thread.currentThread().getName());
             Thread.sleep(new Random().nextInt(2000));
@@ -11,5 +18,13 @@ public class ConnectionResource {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public void take() {
+        taken = true;
+    }
+
+    public void retrieve() {
+        taken = false;
     }
 }
