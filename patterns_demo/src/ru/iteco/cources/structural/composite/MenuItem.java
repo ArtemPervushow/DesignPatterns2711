868cc96888fc89aeff54867b414d086e09e08ac0
mmn222 <ivanb.new@yandex.ru>
2021-11-14 23:07:26 +0300

    Lab3

---
+++ patterns_demo/src/ru/iteco/cources/structural/composite/MenuItem.java
@@ -0,0 +1,15 @@
+package ru.iteco.cources.structural.composite;
+
+public class MenuItem {
+    private String name;
+    private String description;
+    private boolean vegetarian;
+    private double price;
+
+    public MenuItem(String name, String description, boolean vegetarian, double price) {
+        this.name = name;
+        this.description = description;
+        this.vegetarian = vegetarian;
+        this.price = price;
+    }
+}
