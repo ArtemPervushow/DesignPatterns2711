9c94eb5d59f9601ceba590a560c2d14b68b6ba95
mmn222 <ivanb.new@yandex.ru>
2021-11-15 01:03:35 +0300

    Lab6 Command Ivan Bykov - comment added

--- gofpatterns_hometasks_ivanbykov/src/ru/iteco/ibykov/command/banking/ChangeAccountCurrencyOperation.java
+++ gofpatterns_hometasks_ivanbykov/src/ru/iteco/ibykov/command/banking/ChangeAccountCurrencyOperation.java
@@ -7,12 +7,12 @@ public class ChangeAccountCurrencyOperation extends BankOperation{
     }
     @Override
     protected boolean execute() {
-        this.sum=sum/75;
+        this.sum=sum/75; //75 rubles for dollar
         return true;
     }
 
     @Override
     public void undo() {
-        this.sum=sum*75;
+        this.sum=sum*75;//1/75 USD for russian rouble
     }
 }
